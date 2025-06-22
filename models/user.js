64b160f7001aa21f03b199a76364e9bbdad4de4e
Gadz1k <92876034+Gadz1k@users.noreply.github.com>
2025-06-22 04:56:17 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,16 +1,27 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../sequelize');
+const sequelize = require('./sequelize');
 
 const User = sequelize.define('User', {
   username: {
     type: DataTypes.STRING,
     unique: true,
-    allowNull: false
+    allowNull: false,
   },
-  passwordHash: {
+  email: {
     type: DataTypes.STRING,
-    allowNull: false
-  }
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  balance: {
+    type: DataTypes.INTEGER,
+    defaultValue: 1000,
+  },
+}, {
+  timestamps: true,
 });
 
 module.exports = User;
